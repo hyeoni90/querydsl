d04e0b8c72504d68a1947b910284c56e85666f04
hyeoni90 <gusk743@gmail.com>
2020-11-26 23:24:06 +0900

    test: Add JPQL vs Querydsl test code

---
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,69 @@
+package com.study.querydsl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Member;
+import com.study.querydsl.entity.QMember;
+import com.study.querydsl.entity.Team;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by rainalee on 2020-11-26.
+ */
+@SpringBootTest
+@Transactional
+class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    void startJPQL() {
+        String qlString = "select m from Member m "
+            + "where m.username = :username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+            .setParameter("username", "member1")
+            .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void startQuerydsl() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+            .select(m)
+            .from(m)
+            .where(m.username.eq("member1"))
+            .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
