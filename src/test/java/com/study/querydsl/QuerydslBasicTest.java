dc2bb8805b4f616bd3909a546f1939859b763c34
hyeoni90 <gusk743@gmail.com>
2020-11-27 20:44:07 +0900

    test: Add search queydsl test code

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.entity.Member;
+import com.study.querydsl.entity.QMember;
 import com.study.querydsl.entity.Team;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,10 +25,12 @@ class QuerydslBasicTest {
     @Autowired
     EntityManager em;
 
-    JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    JPAQueryFactory queryFactory;
 
     @BeforeEach
     public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -56,6 +60,7 @@ class QuerydslBasicTest {
     }
 
     @Test
+    @DisplayName("기본, static import 사용 (권장)")
     void startQuerydsl() {
         Member findMember = queryFactory
             .select(member)
@@ -65,4 +70,60 @@ class QuerydslBasicTest {
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+    
+    @Test
+    @DisplayName("alias 사용 ")
+    void startQuerydslAlias() {
+        // 같은 테이블 조인해야하는 경우, 아래와 같이 alias 해줘서 사용한다.
+        QMember m1 = new QMember("m1");
+        
+        Member findMember = queryFactory
+            .select(m1)
+            .from(m1)
+            .where(m1.username.eq("member1"))   // 파라미터 바인딩 처리
+            .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void search() {
+        // select().from() >> selectFrom()  으로 사용할 수 있다.
+//            .select(member)
+//            .from(member)
+        
+        Member findMember = queryFactory
+            .selectFrom(member)
+            .where(member.username.eq("member1")
+                .and(member.age.eq(10)))
+            .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void searchBetween() {
+        Member findMember = queryFactory
+            .selectFrom(member)
+            .where(member.username.eq("member1")
+                .and(member.age.between(10, 30)))
+            .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void searchAndParam() {
+        // where 조건 and > 동적 쿼리 짤 때 편리
+        Member findMember = queryFactory
+            .selectFrom(member)
+            .where(
+                member.username.eq("member1"),
+                member.age.eq(10)
+            )
+            .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
 }
