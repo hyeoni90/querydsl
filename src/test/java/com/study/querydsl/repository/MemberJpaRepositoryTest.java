3a728baa0c86af081e7f57e399fdc9593eb66be5
hyeoni90 <gusk743@gmail.com>
2020-12-04 22:12:25 +0900

    feat: 동적쿼리, 성능 최적화 조회 Where 파라미터 사용 production and test code

--- src/test/java/com/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -113,4 +113,34 @@ class MemberJpaRepositoryTest {
         // then
         assertThat(result).extracting("username").containsExactly("member3", "member4");
     }
+
+    @Test
+    void searchTestBySearchWithWhere() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(20);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        // when
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
+
+        // then
+        assertThat(result).extracting("username").containsExactly("member3", "member4");
+    }
 }
